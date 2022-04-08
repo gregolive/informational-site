207e326ab0c7084ba47ed82de26834e6f472a772
Gregory Olive <gregoryolive@outlook.com>
2022-04-08 12:13:49 +0900

    Update server and build index page

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 const port = process.env.PORT || 8080;
-let page;
-
-fs.readFile('./src/index.html', 'utf8' , (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  page = data;
-});
 
 http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(page);
+  // handle request url
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './src/index.html';
+  }
+
+  // match file extension to MIME type
+  let extname = String(path.extname(filePath)).toLowerCase();
+  let mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+  };
+  let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  // respond to client
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    // handle errors
+    if (err) {
+      // reply to ENOENT with 404
+      if(err.code == 'ENOENT') {
+        fs.readFile('./404.html', (err, data) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(data);
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+      }
+    } else {
+      // if no errors send requested file
+      res.statusCode = 200;
+      res.setHeader('Content-Type', contentType);
+      res.end(data);
+    }
+  });
 }).listen(port);
